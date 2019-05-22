11d86ffe58f303a699c75aa43a87fabbb9a27b86
HirokiMiyaoka <hiroki.miyaoka@gmail.com>
2019-05-22 16:04:22 +0900

    Fix.

--- docs/firebase-messaging-sw.js
+++ docs/firebase-messaging-sw.js
@@ -1,4 +1,7 @@
-self.addEventListener('push', ( event ) => { console.log( event );} );
+self.addEventListener('push', ( event ) =>
+{
+	console.log( event, event.data.json() );
+} );
 
 importScripts( 'https://www.gstatic.com/firebasejs/6.0.2/firebase-app.js' );
 importScripts( 'https://www.gstatic.com/firebasejs/6.0.2/firebase-messaging.js' );
